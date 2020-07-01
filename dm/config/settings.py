2720e1d96c1b34837d8051ab5930d938eb8cbbc2
森亮太 <moriryouta@moriryoutanoMacBook-ea.local>
2020-07-01 17:13:11 +0900

    datamodel作成

--- dm/config/settings.py
+++ dm/config/settings.py
@@ -78,12 +78,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+    },
+    'db2' : {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
 
+}
+DATABASE_ROUTERS = ['config.db_router.DbRouter']
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- dm/mail_sending/models.py
+++ dm/mail_sending/models.py
@@ -5,8 +5,9 @@ class Mail(models.Model):
   title = models.CharField(max_length=100)
   content = models.TextField()
   file = models.FileField()
-  
+  start = models.DateTimeField('送信時間')
 
 
   def __str__(self):
-      return self.title
+    start = timezone.localtime(self.start).strftime('%Y/%m/%d %H:%M:%S')
+    return self.title
