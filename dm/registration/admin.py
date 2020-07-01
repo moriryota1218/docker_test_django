602033089c92b31ce36fd66fcece61c4c8ad7e0c
森亮太 <moriryouta@moriryoutanoMacBook-ea.local>
2020-07-01 17:59:56 +0900

    datamodel修正

--- dm/registration/admin.py
+++ dm/registration/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
-from .models import Companies
+from .models import Company
 # Register your models here.
-admin.site.register(Companies)
+admin.site.register(Company)
--- dm/registration/models.py
+++ dm/registration/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Companies(models.Model):
+class Company(models.Model):
   company = models.CharField(max_length=100)
   email = models.EmailField(max_length=254)
   responsible = models.CharField(max_length=100)
